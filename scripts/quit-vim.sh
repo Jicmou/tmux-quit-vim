9e90d2d9db63cc59e0bf9caebd5c98a58b7cee36
jicmou <jc.maigrot@gmail.com>
2025-02-23 13:03:56 +0100

    feat: list all panes with -a flag
    - Adding `-a` flag to `list-panes` command lists all the panes of all
      sessions, so it's not necessary anymore to iterate on sessions and
      windows before listing panes, so we drastically simplify the script.

--- scripts/quit-vim.sh
+++ scripts/quit-vim.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-terminate_vim_in_current_session() {
-  tmux list-panes -F "#{pane_id} #{pane_current_command} #{pane_start_command}" |
+terminate_vim_in_all_panes() {
+  tmux list-panes -aF "#{pane_id} #{pane_current_command} #{pane_start_command}" |
     grep 'vim' |
     awk '/[0-9]+/{ print $1 }' |
     while read -r paneId; do
@@ -11,18 +11,4 @@ terminate_vim_in_current_session() {
     done
 }
 
-terminate_vim_in_windows() {
-  tmux list-windows -F "#{window_id}" | while read -r windowId; do
-    tmux select-window -t"$windowId"
-    terminate_vim_in_current_session
-  done
-}
-
-terminate_vim_in_sessions() {
-  tmux list-sessions -F "#{session_id}" | while read -r sessionId; do
-    tmux switch-client -t"$sessionId"
-    terminate_vim_in_windows
-  done
-}
-
-terminate_vim_in_sessions
+terminate_vim_in_all_panes
