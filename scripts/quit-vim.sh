5f209ce6a9fb00ac1e754bf71bc60e9a911ed205
jicmou <jc.maigrot@gmail.com>
2025-02-23 13:01:46 +0100

    feat: make sure we are in normal mode in (n)vim.
    - Added `send-keys Escape` before quitting all windows to make sure we
      are in NORMAL mode no matter what. I'ts very unlikely we have to press
      Escape twice to go back to NORMAL mode (even though I think it's
      possible, but I never encountered that use case myself).

--- scripts/quit-vim.sh
+++ scripts/quit-vim.sh
@@ -5,6 +5,7 @@ terminate_vim_in_current_session() {
     grep 'vim' |
     awk '/[0-9]+/{ print $1 }' |
     while read -r paneId; do
+      tmux send-keys -t $paneId Escape # Back to normal mode
       tmux send-keys -t $paneId :wqall # Save and quit all windows
       tmux send-keys -t $paneId Enter  # Send Enter to confirm
     done
